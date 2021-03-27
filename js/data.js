5e65c3e33e66e53438e42bb38f1181aaf0cda7bc
DmytroZubkov <69600581+DmytroZubkov@users.noreply.github.com>
2021-03-27 18:34:27 +0200

    01

---
+++ js/data.js
@@ -0,0 +1,198 @@
+﻿let competitors = [
+    {
+        "Position": "1",
+        "Name": "Андрій Доценко",
+        "Shooting": "1+0+0+1",
+        "Total": "2",
+        "Time": "39:31.9"
+    },
+    {
+        "Position": "2",
+        "Name": "Руслан Бригадир",
+        "Shooting": "1+1+0+1",
+        "Total": "3",
+        "Time": "40:32.1",
+        "Time difference": "+1:00.2"
+    },
+    {
+        "Position": "3",
+        "Name": "Віталій Труш",
+        "Shooting": "4+2+1+1",
+        "Total": "8",
+        "Time": "41:14.8",
+        "Time difference": "+1:42.9"
+    },
+    {
+        "Position": "4",
+        "Name": "Сергій Телень",
+        "Shooting": "2+0+3+2",
+        "Total": "7",
+        "Time": "41:21.0",
+        "Time difference": "+1:49.1"
+    },
+    {
+        "Position": "5",
+        "Name": "Євген Івченко",
+        "Shooting": "3+0+1+1",
+        "Total": "5",
+        "Time": "41:34.5",
+        "Time difference": "+2:02.6"
+    },
+    {
+        "Position": "6",
+        "Name": "Роман Боровик",
+        "Shooting": "3+1+1+2",
+        "Total": "7",
+        "Time": "41:42.5",
+        "Time difference": "+2:10.6"
+    },
+    {
+        "Position": "7",
+        "Name": "Микола Ничипоренко",
+        "Shooting": "2+1+1+3",
+        "Total": "7",
+        "Time": "41:52.3",
+        "Time difference": "+2:20.4"
+    },
+    {
+        "Position": "8",
+        "Name": "Денис Насико",
+        "Shooting": "2+1+2+4",
+        "Total": "9",
+        "Time": "42:07.9",
+        "Time difference": "+2:36.0"
+    },
+    {
+        "Position": "9",
+        "Name": "Владислав Чихар",
+        "Shooting": "2+3+0+2",
+        "Total": "7",
+        "Time": "42:14.8",
+        "Time difference": "+2:42.9"
+    },
+    {
+        "Position": "10",
+        "Name": "Володимир Кошман",
+        "Shooting": "0+2+2+2",
+        "Total": "6",
+        "Time": "42:22.1",
+        "Time difference": "+2:50.2"
+    },
+    {
+        "Position": "11",
+        "Name": "Владислав Гончаревський",
+        "Shooting": "1+1+2+3",
+        "Total": "7",
+        "Time": "42:44.5",
+        "Time difference": "+3:12.6"
+    },
+    {
+        "Position": "12",
+        "Name": "Дмитро Грущак",
+        "Shooting": "2+1+4+3",
+        "Total": "10",
+        "Time": "42:55.9",
+        "Time difference": "+3:24.0"
+    },
+    {
+        "Position": "13",
+        "Name": "Андрій Савоненко",
+        "Shooting": "3+0+0+1",
+        "Total": "4",
+        "Time": "43:14.1",
+        "Time difference": "+3:42.2"
+    },
+    {
+        "Position": "14",
+        "Name": "Олександр Родітелєв",
+        "Shooting": "3+3+1+0",
+        "Total": "7",
+        "Time": "43:22.8",
+        "Time difference": "+3:50.9"
+    },
+    {
+        "Position": "15",
+        "Name": "Максим Заровний",
+        "Shooting": "2+1+1+1",
+        "Total": "5",
+        "Time": "43:56.8",
+        "Time difference": "+4:24.9"
+    },
+    {
+        "Position": "16",
+        "Name": "Андрій Орлик",
+        "Shooting": "5+3+0+4",
+        "Total": "12",
+        "Time": "44:39.8",
+        "Time difference": "+5:07.9"
+    },
+    {
+        "Position": "17",
+        "Name": "Юрій Ситник",
+        "Shooting": "1+2+3+1",
+        "Total": "7",
+        "Time": "45:20.8",
+        "Time difference": "+5:48.9"
+    },
+    {
+        "Position": "18",
+        "Name": "Сергій Лютий",
+        "Shooting": "0+0+2+1",
+        "Total": "3",
+        "Time": "45:25.2",
+        "Time difference": "+5:53.3"
+    },
+    {
+        "Position": "19",
+        "Name": "Олег Лисенко",
+        "Shooting": "2+2+2+0",
+        "Total": "6",
+        "Time": "45:30.5",
+        "Time difference": "+5:58.6"
+    },
+    {
+        "Position": "20",
+        "Name": "Ілля Дьков",
+        "Shooting": "1+2+4+3",
+        "Total": "10",
+        "Time": "46:18.7",
+        "Time difference": "+6:46.8"
+    },
+    {
+        "Position": "21",
+        "Name": "Богдан Буров",
+        "Shooting": "2+2+2+3",
+        "Total": "9",
+        "Time": "46:59.2",
+        "Time difference": "+7:27.3"
+    },
+    {
+        "Position": "22",
+        "Name": "Вадим Болдесов",
+        "Shooting": "0+3+1+3",
+        "Total": "7",
+        "Time": "47:19.8",
+        "Time difference": "+7:47.9"
+    },
+    {
+        "Position": "0",
+        "Name": "Артем Тищенко",
+        "Shooting": "0+0+0+0",
+        "Total": "0",
+        "Time": "DNS"
+    },
+    {
+        "Position": "0",
+        "Name": "Владислав Романчич",
+        "Shooting": "0+0+0+0",
+        "Total": "0",
+        "Time": "DNS"
+    },
+    {
+        "Position": "0",
+        "Name": "Владислав Горбащенко",
+        "Shooting": "0+0+0+0",
+        "Total": "0",
+        "Time": "DNS"
+    }
+]
---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+// https://stackoverflow.com/questions/7346563/loading-local-json-file/21446426#21446426
+// function loadFile() {
+//     var input, file, fr;
+
+//     if (typeof window.FileReader !== 'function') {
+//         alert("The file API isn't supported on this browser yet.");
+//         return;
+//     }
+
+//     input = document.getElementById('fileinput');
+//     if (!input) {
+//         alert("Um, couldn't find the fileinput element.");
+//     }
+//     else if (!input.files) {
+//         alert("This browser doesn't seem to support the `files` property of file inputs.");
+//     }
+//     else if (!input.files[0]) {
+//         alert("Please select a file before clicking 'Load'");
+//     }
+//     else {
+//         file = input.files[0];
+//         fr = new FileReader();
+//         fr.onload = receivedText;
+//         fr.readAsText(file);
+//     }
+
+//     function receivedText(e) {
+//         let lines = e.target.result;
+//         let newArr = JSON.parse(lines);
+//         console.log(newArr);
+
+//         let table = document.getElementById('results-body');
+//         newArr.forEach(function (object) {
+//             let tr = document.createElement('tr');
+//             tr.innerHTML = '<td>' + object.Position + '</td>' +
+//                 '<td>' + object.Name + '</td>' +
+//                 '<td>' + object.Shooting + '</td>' +
+//                 '<td>' + object.Total + '</td>' +
+//                 '<td>' + object.Time + '</td>' +
+//                 '<td>' + object['Time difference'] + '</td>';
+//             table.appendChild(tr);
+//         });
+//     }
+// }
+
+let headerArr = []
+competitors.forEach(arrObject => {
+    let x = Object.getOwnPropertyNames(arrObject);
+    x.forEach(el => {
+        if (!headerArr.includes(el)) {
+            headerArr.push(el);
+        }
+    });
+});
+
+let header = document.getElementById('results-header');
+let th = document.createElement('tr');
+th.innerHTML = `<th>${headerArr[0]}</th>
+<th>${headerArr[1]}</th>
+<th>${headerArr[2]}</th>
+<th>${headerArr[3]}</th>
+<th>${headerArr[4]}</th>
+<th>${headerArr[5]}</th>`;
+header.appendChild(th);
+
+let table = document.getElementById('results-body');
+competitors.forEach(function (object) {
+    let tr = document.createElement('tr');
+    tr.innerHTML = `<td>${object.Position}</td>
+    <td>${object.Name}</td>
+    <td>${object.Shooting}</td>
+    <td>${object.Total}</td>
+    <td>${object.Time}</td>
+    <td>${object['Time difference'] == undefined ? "" : object['Time difference']}</td>`;
+    table.appendChild(tr);
+});
